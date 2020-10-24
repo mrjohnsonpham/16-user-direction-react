0e292f3ab2b2e268a70af45071cc090d2aa02635
Johnson Pham <mrjohnsonpham@gmail.com>
2020-10-24 14:28:37 -0400

    fixing deployment

---
+++ build/precache-manifest.66784c554c5074564f7edb7694fa9651.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "167830b3676aebee4f949c0284949e37",
+    "url": "/16-user-direction-react/index.html"
+  },
+  {
+    "revision": "8d84935b464ebff92546",
+    "url": "/16-user-direction-react/static/css/main.6e885ce6.chunk.css"
+  },
+  {
+    "revision": "20066b462c6ded07abed",
+    "url": "/16-user-direction-react/static/js/2.9cd15b15.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/16-user-direction-react/static/js/2.9cd15b15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d84935b464ebff92546",
+    "url": "/16-user-direction-react/static/js/main.db0c8d13.chunk.js"
+  },
+  {
+    "revision": "06f053d21a48efd3d0ab",
+    "url": "/16-user-direction-react/static/js/runtime-main.51dcffbb.js"
+  }
+]);
